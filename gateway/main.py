f5a4e3960c8eab28c11df6320a4b65ad0fd528dc
KrystianMand <krystianmand245@gmail.com>
2024-05-21 19:54:14 +0200

    XDD

--- gateway/main.py
+++ gateway/main.py
@@ -8,16 +8,17 @@ baud_rate = 115200
 
 ser = serial.Serial(port, baud_rate)
 
-res = requests.get('http://127.0.0.1:8000/endpoints/animations/0')
+res = requests.get('http://192.168.114.185:8000/endpoints/animations/0')
 
 rgb_values = literal_eval(res.json()['rgb_values'])
 
-for frame in rgb_values:
-    print('NEW FRAME')
-    for i in range(16):
-        data_to_send = str(frame[16 * i:16 * i + 16]) + '\n'
-        data_to_send = data_to_send.replace("[", "").replace("]", "").replace(" ", "")
-        print(data_to_send)
-        ser.write(data_to_send.encode())
-        sleep(0.1)
-    sleep(1)
+while True:
+    for frame in rgb_values:
+        print('NEW FRAME')
+        for i in range(16):
+            data_to_send = str(frame[16 * i:16 * i + 16]) + '\n'
+            data_to_send = data_to_send.replace("[", "").replace("]", "").replace(" ", "")
+            print(data_to_send)
+            ser.write(data_to_send.encode())
+            sleep(0.1)
+        # sleep(1)
